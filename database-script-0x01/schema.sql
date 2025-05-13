533b0210f8ffdb93d47c8373354a264ab78f1026
Mitchkal <mitchellkalenda@gmail.com>
2025-05-13 13:15:34 +0300

    fixed error where User table name was used instead of users, Removed index creation on attributes that are already foreign keys since mySQL automatically indexes them on foreign key creation

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,5 +1,5 @@
---users table
-CREATE TABLE users (
+-- users table
+CREATE TABLE IF NOT EXISTS users (
     user_id CHAR(36) PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_user_user_id ON users(user_id);
+-- CREATE INDEX idx_user_user_id ON users(user_id);
 
 -- properties table
-CREATE TABLE properties (
+CREATE TABLE IF NOT EXISTS properties (
     property_id CHAR(36) PRIMARY KEY,
     host_id CHAR(36) NOT NULL,
     FOREIGN KEY (host_id) REFERENCES users(user_id),
@@ -25,10 +25,10 @@ CREATE TABLE properties (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_property_property_id ON properties(property_id);
+-- CREATE INDEX idx_property_property_id ON properties(property_id);
 
 -- bookings table
-CREATE TABLE bookings (
+CREATE TABLE IF NOT EXISTS bookings (
     booking_id CHAR(36) PRIMARY KEY,
     property_id CHAR(36) NOT NULL,
     user_id CHAR(36) NOT NULL,
@@ -41,11 +41,11 @@ CREATE TABLE bookings (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_booking_property_id ON bookings(property_id);
-CREATE INDEX idx_booking_booking_id ON bookings(booking_id);
+-- CREATE INDEX idx_booking_property_id ON bookings(property_id);
+-- CREATE INDEX idx_booking_booking_id ON bookings(booking_id);
 
 -- payments table
-CREATE TABLE payments (
+CREATE TABLE IF NOT EXISTS payments (
     payment_id CHAR(36) PRIMARY KEY,
     booking_id CHAR(36) NOT NULL,
     FOREIGN KEY(booking_id) REFERENCES bookings(booking_id),
@@ -54,22 +54,22 @@ CREATE TABLE payments (
     payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL
 );
 
-CREATE INDEX idx_payment_booking_id ON payments(booking_id);
+-- CREATE INDEX idx_payment_booking_id ON payments(booking_id);
 
 -- reviews table
-CREATE TABLE reviews (
+CREATE TABLE IF NOT EXISTS reviews (
     review_id CHAR(36) PRIMARY KEY,
     property_id CHAR(36) NOT NULL,
     FOREIGN KEY(property_id) REFERENCES properties(property_id),
     user_id CHAR(36) NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES User(user_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
     rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
     comment TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- messages table
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
     message_id CHAR(36) PRIMARY KEY,
     sender_id CHAR(36) NOT NULL,
     FOREIGN KEY (sender_id) REFERENCES users(user_id),
