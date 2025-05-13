64bb57f7bce797944088c04bd5d8fb1e9485713a
Mitchkal <mitchellkalenda@gmail.com>
2025-05-13 13:50:35 +0300

    modified left join to utilize ORDER by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,7 +3,6 @@
 SELECT * FROM bookings INNER JOIN users ON bookings.user_id = users.user_id;
 
 -- using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
-SELECT * FROM properties LEFT JOIN reviews ON properties.property_id = reviews.property_id WHERE reviews.review_id IS NULL;
-
+SELECT * FROM properties LEFT JOIN reviews ON properties.property_id = reviews.property_id ORDER By properties.name;
 --  using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT * FROM users LEFT OUTER JOIN bookings ON users.user_id = bookings.user_id;
