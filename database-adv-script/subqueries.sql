7320b0dac24e08b567cf6c2af57780056628acf0
Mitchkal <mitchellkalenda@gmail.com>
2025-05-14 22:23:34 +0300

    added subqueries to retriev users with more than 3 bookings and properties with a rating more than 4

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,6 @@
+-- sql
+-- query to find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT * FROM properties WHERE property_id IN (SELECT property_id FROM reviews WHERE rating > 4);
+
+-- a correlated subquery to find users who have made more than 3 bookings.
+SELECT * FROM users u WHERE ( SELECT count(*) FROM bookings b WHERE u.user_id = b.user_id) > 3;
