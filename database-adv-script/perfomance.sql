a5dd812349b5406d4a7a6693d4bedccd761ef93d
Mitchkal <mitchellkalenda@gmail.com>
2025-05-16 13:39:48 +0300

    deleted performance.sql and added WHERE and AND complexity in the queries in perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -3,7 +3,7 @@ EXPLAIN SELECT *
  FROM users
  INNER JOIN bookings ON users.user_id =  bookings.user_id 
  INNER JOIN properties ON bookings.property_id = properties.property_id
- INNER JOIN payments ON bookings.booking_id = payments.booking_id;
+ INNER JOIN payments ON bookings.booking_id = payments.booking_id WHERE payment_method LIKE 'paypal' AND amount > 0.0;
 
 --- improved query to make it more efficient
 SELECT
@@ -22,4 +22,5 @@ FROM bookings AS b
 INNER JOIN users AS u ON b.user_id = u.user_id
 INNER JOIN properties AS p ON b.property_id = p.property_id
 INNER JOIN payments AS pay ON b.booking_id = pay.booking_id
+WHERE payment_method LIKE 'paypal' AND amount > 0.0
 ORDER BY b.start_date DESC;
